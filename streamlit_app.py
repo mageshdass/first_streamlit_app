bf52e92376c7ba62fee2944c509fec2490fd8479
mageshdass <34598861+mageshdass@users.noreply.github.com>
2023-03-20 18:49:10 +0530

    Update streamlit_app.py
    added toast and new header option

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,11 @@ streamlit.title('My Parents New Healthy Diner')
 
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oat meal')
-streamlit.text('Kale , Spinach E Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Ranged Egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oat meal')
+streamlit.text('🥗 Kale , Spinach E Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Ranged Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
